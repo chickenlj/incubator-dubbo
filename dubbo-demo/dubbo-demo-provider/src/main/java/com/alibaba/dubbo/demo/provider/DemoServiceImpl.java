ff3229a957b0ca55814a938741587c2de4c84ab7
ken.lj <ken.lj.hz@gmail.com>
2018-05-23 16:16:08 +0800

    Demo for fallback

--- dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/DemoServiceImpl.java
+++ dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/DemoServiceImpl.java
@@ -21,11 +21,22 @@ import com.alibaba.dubbo.rpc.RpcContext;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 public class DemoServiceImpl implements DemoService {
 
+    private Random r = new Random(1);
+
     public String sayHello(String name) {
         System.out.println("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext.getContext().getRemoteAddress());
+        int i = r.nextInt(4);
+        if (i <= 2) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         return "Hello " + name + ", response form provider: " + RpcContext.getContext().getLocalAddress();
     }
 
