0f310c6b5d1f394444a4234549ac62262c23e505
ken.lj <ken.lj.hz@gmail.com>
2018-08-31 16:02:39 +0800

    asdfsf

--- release.sh
+++ release.sh
@@ -197,7 +197,7 @@ fi
 #if [ $? -ne 0 ] ; then
 #   fail "ERROR: mvn release:perform was not successful"
 #fi
-mvn versions:set versions:commit -DprocessAllModules=true -DnewVersion=$version
+mvn versions:set versions:commit -DprocessAllModules=true -DnewVersion=$version >> $log
 read -p "test"
 #mvn clean install -DskipTests
 cd ./distribution
