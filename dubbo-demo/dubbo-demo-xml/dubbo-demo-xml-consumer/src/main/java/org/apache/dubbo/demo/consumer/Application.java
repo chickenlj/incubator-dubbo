7ee79402224124822b4ed8e49dc8aa65019413c6
zonghaishang <yiji@apache.org>
2021-08-11 11:51:12 +0800

    local test.

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-consumer/src/main/java/org/apache/dubbo/demo/consumer/Application.java
@@ -32,26 +32,26 @@ public class Application {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-consumer.xml");
         context.start();
         DemoService demoService = context.getBean("demoService", DemoService.class);
-        GreetingService greetingService = context.getBean("greetingService", GreetingService.class);
-
-        new Thread(() -> {
-            while (true) {
-                String greetings = greetingService.hello();
-                System.out.println(greetings + " from separated thread.");
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
+//        GreetingService greetingService = context.getBean("greetingService", GreetingService.class);
+//
+//        new Thread(() -> {
+//            while (true) {
+//                String greetings = greetingService.hello();
+//                System.out.println(greetings + " from separated thread.");
+//                try {
+//                    Thread.sleep(100);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
 
         while (true) {
             CompletableFuture<String> hello = demoService.sayHelloAsync("world");
             System.out.println("result: " + hello.get());
 
-            String greetings = greetingService.hello();
-            System.out.println("result: " + greetings);
+//            String greetings = greetingService.hello();
+//            System.out.println("result: " + greetings);
 
             Thread.sleep(500);
         }
